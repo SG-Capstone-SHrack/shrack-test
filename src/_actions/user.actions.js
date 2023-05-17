9bbbf91d8a3d97b0671a5d868d727209f6cb4037
suminyu <suminei@naver.com>
2023-05-17 17:28:36 +0900

    create project

--- src/_actions/user.actions.js
+++ src/_actions/user.actions.js
@@ -1,7 +1,8 @@
-import { useRecoilState } from 'recoil';
-import { useSetRecoilState, useResetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState, useResetRecoilState } from 'recoil';
+
 import { history, useFetchWrapper } from '_helpers';
 import { authAtom, usersAtom, userAtom } from '_state';
+
 export { useUserActions };
 
 function useUserActions () {
@@ -10,6 +11,7 @@ function useUserActions () {
     const [auth, setAuth] = useRecoilState(authAtom);
     const setUsers = useSetRecoilState(usersAtom);
     const setUser = useSetRecoilState(userAtom);
+
     return {
         login,
         logout,
@@ -21,53 +23,65 @@ function useUserActions () {
         resetUsers: useResetRecoilState(usersAtom),
         resetUser: useResetRecoilState(userAtom)
     }
+
     function login({ username, password }) {
         return fetchWrapper.post(`${baseUrl}/authenticate`, { username, password })
             .then(user => {
-                // 페이지 고침 전 로컬스토리지에 저장
-                localStorage.setItem('user', JSON.stringify(user)); //json데이터로 변경
+                //1) 사용자 정보 로컬 스토리지에 저장 
+                localStorage.setItem('user', JSON.stringify(user));
                 setAuth(user);
+
+                //2) 메인 홈으로 이동(main service)
                 const { from } = history.location.state || { from: { pathname: '/' } };
                 history.push(from);
             });
     }
+
     function logout() {
-        //로컬스토리지에서 없애고 null 상태로 바꾸기 -> 로그인 페이지로 이동
+        // 로그아웃 함수 : 로컬 스토리지에서 삭제 -> 로그인 페이지로 이동 
         localStorage.removeItem('user');
         setAuth(null);
         history.push('/account/login');
     }
+
     function register(user) {
         return fetchWrapper.post(`${baseUrl}/register`, user);
     }
+
     function getAll() {
         return fetchWrapper.get(baseUrl).then(setUsers);
     }
+
     function getById(id) {
         return fetchWrapper.get(`${baseUrl}/${id}`).then(setUser);
     }
-    function update(id, params) { // 업데이트 하기
+
+    function update(id, params) {
         return fetchWrapper.put(`${baseUrl}/${id}`, params)
             .then(data => {
                 if (id === auth?.id) {
                     const user = { ...auth, ...params };
-                    localStorage.setItem('user', JSON.stringify(user));
+                    localStorage.setItem('user', JSON.stringify(user)); //로컬 스토리지 정보 업데이트
                     setAuth(user);
                 }
                 return data;
             });
     }
 
-    function _delete(id) {  //데이터 삭제 함수
-        setUsers(users => users.map(x => {
-            if (x.id === id) 
-                return { ...x, isDeleting: true };
-            return x;
+    // 회원 정보 삭제
+    function _delete(id) {
+        setUsers(users => users.map(data => {
+            if (data.id === id) 
+                return { ...data, isDeleting: true };
+
+            return data;
         }));
         return fetchWrapper.delete(`${baseUrl}/${id}`)
             .then(() => {
-                setUsers(users => users.filter(data_3 => data_3.id !== id));
-                if (id === auth?.id) {
+                // delete -> 이후 리스트에서 아예 없애기
+                setUsers(users => users.filter(x => x.id !== id));
+                // 로그아웃 함수 호출
+                if (id === auth?.id) { 
                     logout();
                 }
             });
